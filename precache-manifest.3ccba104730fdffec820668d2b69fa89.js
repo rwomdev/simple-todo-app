cf240952d6d0d54c796eb72dd62c73bac43a0f2f
rwomdev <rwomdev@gmail.com>
2020-08-15 22:44:26 -0700

    Updates

---
+++ precache-manifest.3ccba104730fdffec820668d2b69fa89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2716df0e869e66377d909de9a8a295",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "d33513a0cf9f2367a108",
+    "url": "/simple-todo-app/static/css/main.74c5bf8e.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33513a0cf9f2367a108",
+    "url": "/simple-todo-app/static/js/main.b756aa7f.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
